11abe123485262c5d6937421fa066a5f737c0971
2ue <jie746635835@163.com>
2017-08-03 11:28:31 +0800

    add gitignore file and http proxy

--- fe/config/index.js
+++ fe/config/index.js
@@ -11,7 +11,7 @@ module.exports = {
     // assetsSubDirectory: 'static',
     assetsSubDirectory: '',
     // assetsPublicPath: '/',
-    assetsPublicPath: '/static',
+    assetsPublicPath: './static',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
@@ -31,7 +31,17 @@ module.exports = {
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: { },
+    proxyHttp: {
+        path: '/',
+        options: {
+            target: 'http://127.0.0.1:5000',
+            changeOrigin: true,
+            pathRewrite: {
+              '^/': '/'
+            }
+        }
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
