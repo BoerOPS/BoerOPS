b96f4ef16eebbf934b62fcf7cb07e3eb6cc076b9
boer <boer0924@qq.com>
2017-08-04 17:01:00 +0800

    test gitlab oauth2 & add/del webhook scripts

--- views/users.py
+++ views/users.py
@@ -1,8 +1,40 @@
-from flask import Blueprint
+from flask import Blueprint, redirect, request, jsonify
 from flask_restful import Api, Resource, url_for
+import requests
 
 bp = Blueprint('user', __name__)
 api = Api(bp)
+
+
+client_id = '8d3b6063c3810d8ba1698d44c96454a767232f33a8e6fa23110905310ec4d768'
+secert = '381e4e76b806ddad32f51c238386d1a0ba80fa211b48bf542c6a101cbf44a48f'
+redirect_uri = 'http://webhook.mail.heclouds.com/oauth2/welcome'
+
+
+@bp.route('/oauth2/welcome')
+def oauth2_welcome():
+    code = request.args.get('code')
+    # parameters = 'client_id=APP_ID&client_secret=APP_SECRET&code=RETURNED_CODE&grant_type=authorization_code&redirect_uri=REDIRECT_URI'
+    # RestClient.post 'http://gitlab.example.com/oauth/token', parameters
+    parameters = {
+        'client_id': client_id,
+        'client_secret': secert,
+        'code': code,
+        'grant_type': 'authorization_code',
+        'redirect_uri': redirect_uri
+    }
+    url = 'http://gitlab.onenet.com/oauth/token'
+    r = requests.post(url, params=parameters)
+    return jsonify(r.json())
+
+
+@bp.route('/auth/login')
+def auth_login():
+    # https://gitlab.example.com/oauth/authorize?client_id=APP_ID&redirect_uri=REDIRECT_URI&response_type=code&state=YOUR_UNIQUE_STATE_HASH
+    state = 'gitlab'
+    url = 'http://gitlab.onenet.com/oauth/authorize?client_id=%s&redirect_uri=%s&response_type=code&state=%s' % (client_id, redirect_uri, state)
+    return redirect(url)
+
 
 class User(Resource):
     def get(self, id):
--- views/webhooks.py
+++ views/webhooks.py
@@ -1,11 +1,50 @@
 import json
 
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, jsonify
 
 from models.commits import Commit
 from models.tests import Test
 
 bp = Blueprint('webhook', __name__)
+
+
+import gitlab
+
+hook_url = 'http://webhook.mail.heclouds.com/pushhook'
+
+gl = gitlab.Gitlab('http://gitlab.onenet.com', 'W8jWv6i_X9WRtMRr2xgv')
+
+
+# 添加
+@bp.route('/script/add_pushhook')
+def add_pushhook():
+    res_success = res_already = []
+    for p in gl.projects.list(all=True):
+        per_project_hooks = [hook.url for hook in gl.project_hooks.list(project_id=p.id)]
+        if hook_url not in per_project_hooks:
+            gl.project_hooks.create({
+                'url': hook_url,
+                'push_events': 1,
+                'enable_ssl_verification': 0
+            }, project_id=p.id)
+            res_success.append(p.name)
+        else:
+            res_already.append(p.name)
+    return jsonify(res_success, res_already)
+
+
+# 删除
+@bp.route('/script/del_pushhook')
+def del_pushhook():
+    res_success = res_nohas = []
+    for p in gl.projects.list(all=True):
+        for hook in gl.project_hooks.list(project_id=p.id):
+            if hook.url == hook_url:
+                hook.delete()
+                res_success.append(p.name)
+            else:
+                res_nohas.append(p.name)
+    return jsonify(res_success, res_nohas)
 
 
 @bp.route('/pushhook', methods=['POST'])
