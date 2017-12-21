1b52b49b48ebbf20d8d1b672222a9f75b5073b64
boer <boer0924@qq.com>
2017-12-21 17:28:08 +0800

    add db schema & deploy api

--- views/deploys.py
+++ views/deploys.py
@@ -1,11 +1,31 @@
-from flask import Blueprint
-from flask_restful import Api, Resource, url_for
+from flask import Blueprint, g
+from flask_restful import Api, Resource, reqparse
+
+from models.projects import Project as ProjectModel
+from models.hosts import Host as HostModel
 
 bp = Blueprint('deploy', __name__)
 api = Api(bp)
 
+parser = reqparse.RequestParser()
+
+
 class Deploy(Resource):
     def get(self, id):
         return {'task': 'done'}
+
+
+class DeployList(Resource):
+    def get(self):
+        pass
+
+    def post(self):
+        parser.add_argument('project_id', help='required')
+        parser.add_argument('branch_id', help='required')
+        parser.add_argument('commit_id', help='required')
+        args = parser.parse_args()
+        _project = g.gl.projects.get(id(args['project_id']))
+        repo_url = _project.attributes.get('repo_url')
+
 
 api.add_resource(Deploy, '/deploy/<int:id>')
